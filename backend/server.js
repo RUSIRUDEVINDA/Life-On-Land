cd6c19e8edf530e7ef65fb2b0deb739d192a8922
R D <rusirud49@gmail.com>
2026-02-10 17:45:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,29 @@
 import { connectDB } from "./config/db.js"
+import cookieParser from "cookie-parser"
 import express from "express"
 import cors from "cors"
+import authRoutes from "./routes/auth.route.js"
 import dotenv from "dotenv"
 
 dotenv.config()
 
 const app = express()
-
 connectDB()
 
+// Enable CORS for all routes
 app.use(cors())
 
+// Middleware to parse JSON bodies
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('This is the backend server for Life on Land')
-})
+// Middleware to parse cookies
+app.use(cookieParser()); 
 
-const PORT = process.env.PORT || 5001
+// Auth routes
+app.use("/api/auth", authRoutes)
+
+const PORT = process.env.PORT || 5001 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
+
