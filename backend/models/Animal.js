e0169d7e4095275543ef95e0a6d0a666c412ffae
Ovindi Vimasha <ovindivimasha1015@gmail.com>
2026-02-13 12:28:27 +0530

    Create patrol routes and controller

---
+++ backend/models/Animal.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const animalSchema = new mongoose.Schema(
+    {
+        tagId: {
+            type: String,
+            required: true,
+            unique: true,
+            index: true,
+            trim: true
+        },
+        species: {
+            type: String,
+            required: true,
+            index: true,
+            trim: true
+        },
+        sex: {
+            type: String,
+            enum: ["MALE", "FEMALE", "UNKNOWN"],
+            required: true,
+            default: "UNKNOWN"
+        },
+        ageClass: {
+            type: String,
+            enum: ["INFANT", "JUVENILE", "SUBADULT", "ADULT", "UNKNOWN"],
+            required: true,
+            default: "UNKNOWN"
+        },
+        protectedAreaId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            index: true
+        },
+        status: {
+            type: String,
+            enum: ["ACTIVE", "INACTIVE", "RETIRED", "DECEASED"],
+            required: true,
+            default: "ACTIVE",
+            index: true
+        },
+    },
+    { timestamps: true }
+);
+
+const Animal = mongoose.model("Animal", animalSchema);
+
+export default Animal;
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import { connectDB } from "./config/db.js"
+import cookieParser from "cookie-parser"
+import express from "express"
+import cors from "cors"
+import authRoutes from "./routes/auth.route.js"
+import patrolRoutes from "./routes/patrol.route.js"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+connectDB()
+
+// Enable CORS for all routes
+app.use(cors())
+
+// Middleware to parse JSON bodies
+app.use(express.json())
+
+// Middleware to parse cookies
+app.use(cookieParser());
+
+// Auth routes
+app.use("/api/auth", authRoutes)
+
+
+// Patrol routes
+app.use("/api/patrols", patrolRoutes)
+
+const PORT = process.env.PORT || 5001
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
