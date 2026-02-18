6bf33d2fa07230f0e56715e8b2e0d1d99d3d4f6c
RUSIRUDEVINDA <rusirud49@gmail.com>
2026-02-18 11:19:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ import express from "express"
 import cors from "cors"
 import authRoutes from "./routes/auth.route.js"
 import animalRoutes from "./routes/animal.route.js"
-import patrolRoutes from "./routes/patrol.route.js"
 import dotenv from "dotenv"
+
 
 dotenv.config()
 
@@ -23,11 +23,9 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes)
 // Animal routes
 app.use("/api/animals", animalRoutes)
-// Patrol routes
-app.use("/api/patrols", patrolRoutes)
-
 
 const PORT = process.env.PORT || 5001
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
+
